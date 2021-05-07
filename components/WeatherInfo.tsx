6fed8baf134964df70cfd7c298eb04c2a242e50f
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-05-07 20:19:10 +0400

    First ready version

--- components/WeatherInfo.tsx
+++ components/WeatherInfo.tsx
@@ -1,8 +1,9 @@
 import styles from '../styles/WeatherInfo.module.scss'
 import Image from 'next/image'
+import WeatherData from '../types/WeatherData'
 
 type AppProps = {
-    data: any
+    data: WeatherData
 }
 
 const WeatherInfo = ({ data }: AppProps): JSX.Element => {
@@ -15,8 +16,19 @@ const WeatherInfo = ({ data }: AppProps): JSX.Element => {
     }
     return (
         <div className={styles.box}>
-            <h1><Image width="100px" height="100px" src="http://openweathermap.org/img/wn/10d@4x.png" /> {data.name} <p>{data.temperature}°C</p></h1>
-
+            <h1><Image width="100px" height="100px" src="http://openweathermap.org/img/wn/10d@4x.png" /> {data.name}, {data.country} <p>{data.temperature} °C</p></h1>
+            <div className={ styles.data_list_fragment }>
+                <ul className={styles.data_list}>
+                    <li><h3>Feels Like:</h3> <p>{data.feels_like} °C</p></li>
+                    <li><h3>Weather:</h3> <p>{data.weather_type}</p></li>
+                    <li><h3>Description:</h3> <p>{data.weather_type_desc}</p></li>
+                </ul>
+                <ul className={styles.data_list}>
+                    <li><h3>Pressure:</h3> <p>{data.pressure} hPa</p></li>
+                    <li><h3>Humidity:</h3> <p>{data.humidity} %</p></li>
+                    <li><h3>Cloudiness:</h3> <p>{data.cloudiness} %</p></li>
+                </ul>
+            </div>
         </div>
     );
 }
